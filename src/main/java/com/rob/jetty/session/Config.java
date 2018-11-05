a43bd1fd75215548ae9ad7dfd084e6e51cc1b140
Rob Benton <rmbenton@arkbluecross.com>
2018-11-05 08:41:37 -0600

    Jetty is running

---
+++ src/main/java/com/rob/jetty/session/Config.java
@@ -0,0 +1,18 @@
+package com.rob.jetty.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Config is the main configuration class for the application.
+ *
+ * @author Rob Benton
+ */
+@SpringBootApplication
+public class Config
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Config.class, args);
+    }
+}
