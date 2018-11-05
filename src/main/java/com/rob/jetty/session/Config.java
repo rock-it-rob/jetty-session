aafcbdad649ee753f4d1a7f5e59a5c135702d098
Rob Benton <rmbenton@arkbluecross.com>
2018-11-05 15:28:22 -0600

    Compiling: true. Working: false.

--- src/main/java/com/rob/jetty/session/Config.java
+++ src/main/java/com/rob/jetty/session/Config.java
@@ -1,7 +1,15 @@
 package com.rob.jetty.session;
 
+import org.eclipse.jetty.nosql.mongodb.MongoSessionDataStore;
+import org.eclipse.jetty.server.session.NullSessionCache;
+import org.eclipse.jetty.server.session.SessionHandler;
+import org.eclipse.jetty.webapp.AbstractConfiguration;
+import org.eclipse.jetty.webapp.WebAppContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Config is the main configuration class for the application.
@@ -14,5 +22,29 @@ public class Config
     public static void main(String[] args)
     {
         SpringApplication.run(Config.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory()
+    {
+        JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
+        factory.addConfigurations(
+            new MyJettyConfig()
+        );
+        return factory;
+    }
+
+
+    static final class MyJettyConfig extends AbstractConfiguration
+    {
+        @Override
+        public void configure(WebAppContext context)
+        {
+            final SessionHandler sessionHandler = context.getSessionHandler();
+            final MongoSessionDataStore dataStore = new MongoSessionDataStore();
+            final NullSessionCache sessionCache = new NullSessionCache(sessionHandler);
+            sessionCache.setSessionDataStore(dataStore);
+            sessionHandler.setSessionCache(new NullSessionCache(sessionHandler)); // no idea if this is right. Seems weird.
+        }
     }
 }
