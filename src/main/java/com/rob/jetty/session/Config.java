5d3f795bdb5f7b32c4151522afec3f91a485744e
Rob Benton <rmbenton@arkbluecross.com>
2018-11-06 10:32:28 -0600

    Ok this is almost working.
    I'm getting some kind of premature read exception from mongo. But the
    configuration is very close I think.

--- src/main/java/com/rob/jetty/session/Config.java
+++ src/main/java/com/rob/jetty/session/Config.java
@@ -1,10 +1,14 @@
 package com.rob.jetty.session;
 
-import org.eclipse.jetty.nosql.mongodb.MongoSessionDataStore;
+import org.eclipse.jetty.nosql.mongodb.MongoSessionDataStoreFactory;
 import org.eclipse.jetty.server.session.NullSessionCache;
+import org.eclipse.jetty.server.session.SessionDataStore;
 import org.eclipse.jetty.server.session.SessionHandler;
 import org.eclipse.jetty.webapp.AbstractConfiguration;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
@@ -19,32 +23,76 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Config
 {
+    private static final Logger log = LoggerFactory.getLogger(Config.class);
+
     public static void main(String[] args)
     {
         SpringApplication.run(Config.class, args);
     }
+
+    @Value("${mongo.host}")
+    private String mongoHost;
+
+    @Value("${mongo.dbname}")
+    private String mongoDbname;
+
+    @Value("${mongo.collection.name}")
+    private String mongoCollectionName;
 
     @Bean
     public ServletWebServerFactory servletWebServerFactory()
     {
         JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
         factory.addConfigurations(
-            new MyJettyConfig()
+            new MongoSessionDataStoreConfig(mongoHost, mongoDbname, mongoCollectionName)
         );
         return factory;
     }
 
 
-    static final class MyJettyConfig extends AbstractConfiguration
+    static final class MongoSessionDataStoreConfig extends AbstractConfiguration
     {
-        @Override
-        public void configure(WebAppContext context)
+        private final String host;
+        private final String dbname;
+        private final String collectionName;
+
+        MongoSessionDataStoreConfig(String host, String dbname, String collectionName)
         {
-            final SessionHandler sessionHandler = context.getSessionHandler();
-            final MongoSessionDataStore dataStore = new MongoSessionDataStore();
+            this.host = host;
+            this.dbname = dbname;
+            this.collectionName = collectionName;
+        }
+
+        @Override
+        public void configure(WebAppContext context) throws Exception
+        {
+            log.debug("Utilizing MongoDB for session data store @ " + host + ". dbname: " + dbname);
+
+            //
+            // TODO: Set SessionIdManager name.
+            // The env variable JETTY_WORKER_INSTANCE must be set.
+            //
+
+
+            SessionHandler sessionHandler = context.getSessionHandler();
+
+            // Create the mongo session data store based on the configured
+            // properties.
+            final MongoSessionDataStoreFactory dataStoreFactory = new MongoSessionDataStoreFactory();
+            dataStoreFactory.setHost(host);
+            dataStoreFactory.setDbName(dbname);
+            dataStoreFactory.setCollectionName(collectionName);
+            log.debug("Using default collection name: " + dataStoreFactory.getCollectionName());
+            final SessionDataStore dataStore = dataStoreFactory.getSessionDataStore(sessionHandler);
+
+
+            // Create the session cache and add the data store to it.
             final NullSessionCache sessionCache = new NullSessionCache(sessionHandler);
             sessionCache.setSessionDataStore(dataStore);
-            sessionHandler.setSessionCache(new NullSessionCache(sessionHandler)); // no idea if this is right. Seems weird.
+
+            // Set the mongo configuration properties.
+            // jetty.session.mongo.host
+            // jetty.session.mongo.dbName
         }
     }
 }
--- src/main/java/com/rob/jetty/session/rs/Application.java
+++ src/main/java/com/rob/jetty/session/rs/Application.java
@@ -3,14 +3,19 @@ package com.rob.jetty.session.rs;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
+import javax.ws.rs.ApplicationPath;
+
 /**
  * Application is the main Jersey {@link ResourceConfig} for the application.
  *
  * @author Rob Benton
  */
 @Component
+@ApplicationPath(Application.PATH)
 public class Application extends ResourceConfig
 {
+    public static final String PATH = "/rs";
+
     public Application()
     {
         register(Health.class);
